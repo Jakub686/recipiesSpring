e42c24a358b4e30ad3b3baa1a68c0855eebdba79
Jakub <jakub.bchyla@gmail.com>
2022-08-08 15:41:06 +0200

    Initial commit

---
+++ src/main/java/com/github/jakub686/recipiesSpring/RecipiesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.jakub686.recipiesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipiesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jakub686/recipiesSpring/RecipiesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jakub686.recipiesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipiesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
